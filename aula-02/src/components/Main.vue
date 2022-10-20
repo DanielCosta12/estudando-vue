aa27e9553048bd0b2383b4e623226158a0512704
Daniel <danieldossantosdacosta@gmail.com>
2022-10-19 23:41:02 -0300

    add lifecycle

--- aula-02/src/components/Main.vue
+++ aula-02/src/components/Main.vue
@@ -3,13 +3,18 @@
   <div>
     <button v-on:click="show">Click here!</button>
     <div id="showAll"></div>
+    <LifeCycle />
   </div>
 </template>
 <script>
+import LifeCycle from "./LifeCycle.vue";
 export default {
   name: "Main",
   data() {
     return {
+      components: {
+        LifeCycle,
+      },
       people: {
         name: "Daniel",
         age: 22,
@@ -21,11 +26,12 @@ export default {
   methods: {
     show: function () {
       const showAll = document.querySelector("#showAll");
-      showAll.innerText = `Meu nome é ${this.people.name}, 
+      showAll.innerText = `Meu nome é ${this.people.name},
       tenho ${this.people.age} anos.
       Sou do ${this.people.from} e moro no estado do ${this.people.state}
       `;
     },
   },
+  components: { LifeCycle },
 };
 </script>
