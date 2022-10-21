8916e1d578d4ff0b68a3cf7e91c987b4efdfaf8b
Daniel <danieldossantosdacosta@gmail.com>
2022-10-20 22:50:13 -0300

    add hieraquia de componentes 1/2

--- aula-02/src/App.vue
+++ aula-02/src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <Main />
+  <div>
+    <Main />
+    <LifeCycle />
+    <Pessoa />
+  </div>
 </template>
 <script>
 import Main from "./components/Main.vue";
+import Pessoa from "./components/Pessoa.vue";
 export default {
   name: "app",
   components: {
     Main,
+    Pessoa,
   },
 };
 </script>
---
+++ aula-02/src/components/Pessoa.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Meu nome é {{ name }}</h1>
+    <p>Atualmente estou estudando:{{ languages[0] }}</p>
+    <p>Pretendo estudar:{{ languages.slice(1) }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "Pessoa",
+
+  data() {
+    return {
+      name: "Daniel",
+      languages: [
+        "Vue",
+        "Bootstrap",
+        "Tailwind",
+        "NextJS",
+        "React",
+        "ShellScript",
+        "C",
+      ],
+    };
+  },
+};
+</script>
